58e550c7b907249b7c2b1cd8b330b43ce8b2bd3c
murloc <81496776+0xmurloc@users.noreply.github.com>
2023-07-05 19:13:09 +0200

    lint

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -4,12 +4,12 @@ export const DEX_V2_SUBGRAPH: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]:
     'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-fuji',
   [ChainId.SPRUCE]:
-    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-fuji',
+    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/joe-v2-fuji'
 }
 
 export const DEXCANDLES_SUBGRAPH_V2: { [chainId in ChainId]: string } = {
   [ChainId.FUJI]:
     'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dex-candles-v2-fuji',
   [ChainId.SPRUCE]:
-    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dex-candles-v2-fuji',
+    'https://api.thegraph.com/subgraphs/name/traderjoe-xyz/dex-candles-v2-fuji'
 }
